fc22bd8e912aff0381ceb83481c94bc5b6c3fc63
Jongyun Won <jywon1128@gmail.com>
2025-12-11 14:48:39 +0900

    feat: dummy reservations flyway

---
+++ resource-server/src/main/resources/db/migration/V2__dummy_reservation.sql
@@ -0,0 +1,23 @@
+INSERT INTO reservations (
+    reservation_head_count,
+    reservation_request,
+    reservation_total_price,
+    reservation_status,
+    user_id,
+    schedule_id
+)
+WITH RECURSIVE seq AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1
+    FROM seq
+    WHERE n < 300000
+)
+SELECT /*+ SET_VAR(cte_max_recursion_depth = 300000) */
+    1 AS reservation_head_count,
+    CONCAT('부하 테스트 예약 ', n) AS reservation_request,
+    100000 AS reservation_total_price,
+    'DEPOSIT_COMPLETED' AS reservation_status,
+    1 AS user_id,
+    1 AS schedule_id
+FROM seq;
