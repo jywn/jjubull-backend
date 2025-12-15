6b607718d6cc1ee9c0cc486e1a0c04f736c32f2d
Jongyun Won <jywon1128@gmail.com>
2025-12-15 16:23:37 +0900

    fix: JPQL grammar

--- resource-server/src/main/java/com/jjubull/resourceserver/schedule/repository/ScheduleJdbcRepository.java
+++ resource-server/src/main/java/com/jjubull/resourceserver/schedule/repository/ScheduleJdbcRepository.java
@@ -16,7 +16,7 @@ public class ScheduleJdbcRepository {
             JOIN ships sh ON s.ship_id = sh.ship_id
             SET s.schedule_current_head_count = s.schedule_current_head_count + ?
             WHERE s.schedule_id = ?
-            AND s.schedule_current_head_count + ? <= sh.max_head_count
+            AND s.schedule_current_head_count + ? <= sh.ship_max_head_count
         """, headCount, scheduleId, headCount);
 
         return updated == 1;
