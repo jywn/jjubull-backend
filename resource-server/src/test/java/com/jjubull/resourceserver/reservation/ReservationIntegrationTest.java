b512a89d21cd6cb56df091558108695d71756a73
Jongyun Won <jywon1128@gmail.com>
2025-12-17 16:07:20 +0900

    feat: test container

--- resource-server/src/test/java/com/jjubull/resourceserver/reservation/ReservationIntegrationTest.java
+++ resource-server/src/test/java/com/jjubull/resourceserver/reservation/ReservationIntegrationTest.java
@@ -21,7 +21,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -29,9 +34,24 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Transactional
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 public class ReservationIntegrationTest {
+
+    @Container
+    static MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.4")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
     @Autowired
     ReservationCommandService reservationCommandService;
